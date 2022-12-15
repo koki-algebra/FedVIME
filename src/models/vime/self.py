22b859334793eae65d5cccb362548d37aba2308e
Koki Yamada <nino02000@icloud.com>
2022-12-15 12:50:14 +0000

    add self & semi network

---
+++ src/models/vime/self.py
@@ -0,0 +1,57 @@
+from typing import Tuple
+
+from torch import nn
+from torch import Tensor, sigmoid
+
+
+class SelfSLNetworks(nn.Module):
+    def __init__(self, dim_x: int, dim_z: int, is_norm=True) -> None:
+        super().__init__()
+        self.is_norm = is_norm
+
+        # encoder e: X -> Z
+        self.encoder = nn.Sequential(
+            nn.BatchNorm1d(dim_x),
+            nn.Linear(dim_x, dim_x*2),
+            nn.ReLU(),
+            nn.BatchNorm1d(dim_x*2),
+            nn.Linear(dim_x*2, dim_x*2),
+            nn.ReLU(),
+            nn.BatchNorm1d(dim_x*2),
+            nn.Linear(dim_x*2, dim_z)
+        )
+
+        # mask vector estimator s_m: Z -> {0,1}^d
+        self.mask_estimator = nn.Sequential(
+            nn.BatchNorm1d(dim_z),
+            nn.Linear(dim_z, dim_z),
+            nn.ReLU(),
+            nn.BatchNorm1d(dim_z),
+            nn.Linear(dim_z, dim_x),
+        )
+
+        # feature vector estimator s_r: Z -> X
+        self.feature_estimator = nn.Sequential(
+            nn.BatchNorm1d(dim_z),
+            nn.Linear(dim_z, dim_z),
+            nn.ReLU(),
+            nn.BatchNorm1d(dim_z),
+            nn.Linear(dim_z, dim_x),
+        )
+
+    def forward(self, X_tilde: Tensor) -> Tuple[Tensor, Tensor]:
+        # encode collapsed feature matrix
+        Z: Tensor = self.encoder(X_tilde)
+        # mask prediction
+        M_pred: Tensor = sigmoid(self.mask_estimator(Z))
+        # feature prediction
+        X_pred: Tensor = self.feature_estimator(Z)
+
+        # is normalization
+        if self.is_norm:
+            X_pred = sigmoid(X_pred)
+
+        return M_pred, X_pred
+
+    def get_encoder(self) -> nn.Module:
+        return self.encoder
---
+++ src/models/vime/semi.py
@@ -0,0 +1,23 @@
+from torch import nn, Tensor
+
+
+class SemiSLNetworks(nn.Module):
+    def __init__(self, encoder: nn.Module, dim_z: int, dim_y: int) -> None:
+        super().__init__()
+        # pre-trained encoder
+        self.encoder = encoder
+        # predictor
+        self.predictor = nn.Sequential(
+            nn.BatchNorm1d(dim_z),
+            nn.Linear(dim_z, dim_z*2),
+            nn.ReLU(),
+            nn.BatchNorm1d(dim_z*2),
+            nn.Linear(dim_z*2, dim_z),
+            nn.ReLU(),
+            nn.Linear(dim_z, dim_y)
+        )
+
+    def forward(self, X: Tensor) -> Tensor:
+        Z = self.encoder(X)
+        logits = self.predictor(Z)
+        return logits
