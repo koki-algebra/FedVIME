d16be7ee05fa542b5e3dba5591b252a308c02b76
Koki Yamada <nino02000@icloud.com>
2022-12-17 09:45:25 +0000

    add MLP

--- src/main.py
+++ src/main.py
@@ -1,2 +1,34 @@
+import warnings
+
+from torch import nn, Tensor
+import easyfl
+
+from datasets import uci_income
+
+
+class MLP(nn.Module):
+    def __init__(self, feature_num: int, class_num: int) -> None:
+        super().__init__()
+
+        self.mlp = nn.Sequential(
+            nn.Linear(feature_num, feature_num),
+            nn.ReLU(),
+            nn.Linear(feature_num, class_num),
+        )
+
+    def forward(self, X: Tensor) -> Tensor:
+        logits = self.mlp(X)
+        return logits
+
+
 if __name__ == "__main__":
-    print("Hello, Federated Learning!!")
+    warnings.simplefilter("ignore")
+
+    train_data, test_data = uci_income.get_dataset(labeled_size=1.0)
+
+    easyfl.register_dataset(train_data, test_data)
+    easyfl.register_model(model=MLP(feature_num=105, class_num=2))
+
+    easyfl.init()
+
+    easyfl.run()
